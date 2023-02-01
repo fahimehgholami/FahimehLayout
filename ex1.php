c91dfd6c504b2b449c8ae64c8410aa2310061ba2
fahimehgholami <114288759+fahimehgholami@users.noreply.github.com>
2023-02-01 18:15:08 +0200

    edited

--- ex1.php
+++ ex1.php
@@ -1,31 +1,35 @@
 <?php 
 $title = "Exersice1";
-include("header.php")?>
-
-    <h1 style="color: orange; font-size: 18px;">Introduction </h1>
+include "header.php" ?>
+<!--3.1-->
+<h1>Write a simple PHP script to print your information. </h1>
 <?php
 //opening tag
 echo "Hello World! <br> ";
 echo "Here's some information \"line\".";
 ?>
 
-<h1 style="color: orange; font-size: 18px;"> 3.2. Write PHP code to display the following message.</h1>
+<!--3.2-->
+<h1>Write PHP code to display the following message.</h1>
 <?php
 echo "Hello World! My name is \"Fahimeh\" ";
 ?>
 
-<h1 style="color: orange;font-size: 18px;"> Current Date </h1>
+<!--3.3-->
+<h1> Current Date </h1>
 <?php 
 echo date("m.d.Y")
 ?>
 
-<h3 style="color: darkblue; font-size: 18px;">3.4 varibles in php</h3>
+<!--3.4-->
+<h3>varibles in php</h3>
 <?php 
-$title1="PHP is interesting";
+$title1="PHP is interesting!";
 echo "<h1>" . $title1 . "</h1>";
 ?>
 
-<h3 style="color: orange; font-size: 18px;"> 3.5 Table & Variables</h3>
+<!--3.5-->
+<h3>Table & Variables</h3>
 <?php
 $g1 = 5;
 $g2 = 4;
@@ -33,25 +37,31 @@ $g3 = 5;
 echo "
 <table>
 <tr>
-<th> S.N </th> <th> Name </th> <th> Grade </th>
+<th> S.N </th>
+ <th> Name  </th>
+  <th> Grade </th>
 </tr>
 <tr>
-<th> 1 </th> <th> Fahimeh </th> <th> $g1 </th>
+<th> 1 </th> 
+<th> Fahimeh </th> 
+<th> $g1 </th>
 </tr>
 <tr>
-<th> 1 </th> <th> Ferdous </th> <th> $g2 </th>
+<th> 1 </th> 
+<th> Ferdous </th> 
+<th> $g2 </th>
 </tr>
 <tr>
-<th> 1 </th> <th> John </th> <th> $g3 </th>
+<th> 1 </th>
+ <th> John </th> 
+ <th> $g3 </th>
 </tr>
 </table>
 ";
 ?>
-
-<h3 style="color: orange; font-size: 18px;"> 4 screenshots of the development invironment</h3>
-<img style="width: 100%; height: 400px;" src="colors.webp" alt="">
+<h3> Take a screenshot </h3>
 
 
-<?php include("footer.php")?>
 
+<?php include "footer.php" ?>
 
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$title = "Home";
+include"header.php" ?>
+<h1> h1 home index phph</h1>
+
+<ul class="list list-flush">
+    <li class="list-items"> jgghsgh</li>
+    <li class="list-items">fhgi</li>
+    <li class="list-items">jfojgoh</li>
+    <li class="list-items">huuig</li>
+</ul>
+<ul class="p_list">
+    <li>higkg</li>
+    <li>hioh</li>
+    <li>pohio</li>
+    <li>ihiug</li>
+</ul>
+<?php include "footer.php" ?>

b4d3c5c3f65de6b6a4fa22f64bcc5c5d62be7de0
fahimehgholami <114288759+fahimehgholami@users.noreply.github.com>
2023-02-07 13:02:08 +0200

    new part added

--- variable.php
+++ variable.php
@@ -1,4 +1,6 @@
 <?php include "header.php" ?>
+
+<!-- form -->
 <h3>
     Exxercise 3 In-class Task Variable & Operators
 </h3>
@@ -28,7 +30,8 @@
 
 </div>
 </form>
-
+<br>
+<!-- table -->
  <h3>Table</h3>
  <h5>Prepare a simple html table and apply bootstrap style to the table. </h5>
  <table class="table table-success table-striped">
@@ -58,6 +61,32 @@ $g3 = 5;
     <td><?php echo $g3?></td>
   </tr>
 </table> 
+<br>
+<!--  string function -->
+<h3> PHP Script</h3>
+<h5> Write a PHP script with two string variables. Assign any text to these variables. Join them together.  Print the length of the string. (Hint: string function)
+</h5>
+<h5>The length of the string:</h5>
+<?php
+$text1= "Versions of the Lorem ipsum text have been used in typesetting at least since the 1960s, when it was popularized by advertisements for Letraset transfer sheets. Lorem ipsum was introduced to the digital world in the mid-1980s, when Aldus employed it in graphic and word-processing templates for its desktop publishing program PageMaker. ";
+$text2= "Other popular word processors, including Pages and Microsoft Word, have since adopted Lorem ipsum, as have many LaTeX packages, web content managers such as Joomla! and WordPress, and CSS libraries such as Semantic UI.";
+$text3= $text1.$text2;
+echo strlen($text3);
+?>
+<br>
 
+<!--   Add up the numbers -->
+<h3>Add up the numbers</h3>
+<h5>Write a script to add up the numbers: 298, 234, 46. Use variables to store these numbers and echo statement to output your answer.
+</h5>
+
+<?php 
+$number1= 298 ;
+$number2= 234 ;
+$number3= 46 ;
+$total=$number1 + $number2 + $number3 ;
+echo "Numbers: 298, 234 and 46, Total = $total";
+?>
+<br>
 
 <?php include "footer.php" ?>

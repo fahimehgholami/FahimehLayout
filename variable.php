ea94fa9ae63f5e176f611c5bbd11c218012aeb96
fahimehgholami <114288759+fahimehgholami@users.noreply.github.com>
2023-02-07 09:36:18 +0200

    edited

--- variable.php
+++ variable.php
@@ -3,7 +3,7 @@
     Exxercise 3 In-class Task Variable & Operators
 </h3>
 <h2>Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print using tag: Hello …., You are welcome to my site. </h2>
-<from> 
+<from action="action.php" method="post"> 
     First Name: <input type="text" name="fname" > <br>
     Last Name: <input type="text" name="lname" > <br>
     Birth Date:  <input type="date" name="bdate"> <br>

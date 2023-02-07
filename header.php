70d0239b0d812dba4d700b5c645c9618d3f94e63
fahimehgholami <114288759+fahimehgholami@users.noreply.github.com>
2023-02-07 09:28:17 +0200

    edited

--- header.php
+++ header.php
@@ -49,17 +49,19 @@
             <li class="nav-item">
               <a class="nav-link" href="ex1.php"><b>Exercise 1</b></a>
             </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="variable.php"><b>
+                
+              Exercise 2
+              </b></a>
+            </li>
             <li class="nav-item">
               <a class="nav-link" href="aboutus.php"><b>About Us</b></a>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link" href="contactus.php"><b>Contact Us</b></a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link" href="variable.php"><b>
-                exercise 2
-              </b></a>
-            </li>
+           
           </ul>
         </div>
       </div>

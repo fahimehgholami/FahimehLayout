db21ed4ec4f9b6ad9a5549718964fd1f48b5921b
fahimehgholami <114288759+fahimehgholami@users.noreply.github.com>
2023-02-07 09:24:52 +0200

    edited

--- variable.php
+++ variable.php
@@ -4,7 +4,12 @@
 </h3>
 <h2>Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print using tag: Hello …., You are welcome to my site. </h2>
 <from> 
-    First Name: <input type="text" name="fname" >
+    First Name: <input type="text" name="fname" > <br>
+    Last Name: <input type="text" name="lname" > <br>
+    Birth Date:  <input type="date" name="bdate"> <br>
+    Select Your Favorite Color:  <input type="color" name="Color"> <br>
+    <input type="submit" value="Submit"> 
+
 </from>
 
 <?php include"footer.php" ?>

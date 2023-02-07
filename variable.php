686664233a525dc1d5b29ff5103b3c231620616c
fahimehgholami <114288759+fahimehgholami@users.noreply.github.com>
2023-02-07 09:20:32 +0200

    variable part

---
+++ variable.php
@@ -0,0 +1,10 @@
+<?php include"header.php" ?>
+<h3>
+    Exxercise 3 In-class Task Variable & Operators
+</h3>
+<h2>Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print using tag: Hello …., You are welcome to my site. </h2>
+<from> 
+    First Name: <input type="text" name="fname" >
+</from>
+
+<?php include"footer.php" ?>

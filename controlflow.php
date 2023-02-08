cc598138f14065343036ca8415c01e77af3445c7
fahimehgholami <114288759+fahimehgholami@users.noreply.github.com>
2023-02-08 11:12:12 +0200

    if else statement practie

--- controlflow.php
+++ controlflow.php
@@ -1,4 +1,33 @@
 <?php include "header.php" ?>
 
+<!--if else statement-->
+<h5>if/else </h5>
+<?php
+$a = 6;
+if ($a == 5){
+    echo "The Condition Is TRUE";
+}
+else {
+    echo "The Condition Is FALSE <br>";
+    echo "The Number Is $a <br>";
+}
+?>
+<br>
+<!--Date Function -->
+<h5> if else statement with date function</h5>
+<?php
+$d = date("D");
+if ($d == "Sat" or $d == "Sun") {
+    echo "<br>Today Is $d. It Is Weekend.";
+}
+else {
+    echo "Today Is $d. It Is a Working Day.<br>";
+}
+?>
+<br>
+
+<!--if else if statement-->
+
+
 
 <?php include "footer.php" ?>

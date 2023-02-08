59f1aa2022204ab6b3fa297e31655079f80c1f9e
fahimehgholami <114288759+fahimehgholami@users.noreply.github.com>
2023-02-08 16:00:31 +0200

    vote form added

--- action.php
+++ action.php
@@ -1,13 +1,25 @@
-<?php include "header.php";
+<?php include "header.php"?>
 
-$fname =$_POST['fname'];
-$lname =$_POST['lname'];
-$dob =$_POST['bdate'];
-$color =$_POST['color'];
-echo " <h3> WELCOME $fname $lname. <br>
-<br>Your Favorite color is:$color <br>
-<br>Your date of bitrh is: $dob </h3><br> "
+<?php
+
+//Create a simple html form in variable.php
+if (isset($_POST['Submit'])){
+    $fname =$_POST['fname'];
+    $lname =$_POST['lname'];
+    $dob =$_POST['bdate'];
+    $color =$_POST['color'];
+    echo " <h3> WELCOME $fname $lname. <br>
+    <br>Your Favorite color is:$color <br>
+    <br>Your date of bitrh is: $dob </h3><br> ";
+}
+
+
+// Write a program to get inputs (age and name) from the user and based on their age, decide if he/she is eligible for voting. 
+ elseif (isset($_POST['Submit1'])) {
+$firstname = $_POST['firstname'];
+$age = $_POST['age'];
+
+echo $age >= 18 ? "<h3>congratulations $firstname!<br> <br>You are eligble to VOTE." : " Sorry $firstname!<br><br>  You are not eligble to VOTE!</h3>";
+ }
 ?>
-
-
 <?php include "footer.php" ?>
--- controlflow.php
+++ controlflow.php
@@ -45,10 +45,31 @@ if ($score > 80) {
 }
 ?>
 <!--4.4-->
+<?php
+echo "<hr><h5>Write a program to get inputs (age and name) from the user and based on their age, decide if he/she is eligible for voting</h5>";
+?>
 
-
+<form action="action.php" method="post">
+  <div class="row">
+    <div class="col">
+      <input class="form-control" type="text" placeholder="First Name" name="firstname" required><br>
+    </div>
+    <div class="col">
+      <input class="form-control" type="number" placeholder="Age" name="age" required><br>
+    </div>
+  </div>
+  <div class="row">
+    <input class="form-control" type="submit" value="Submit" name="Submit1" >
+  </div>
+</form>
+<!-- end of 4.4-->
 
 <!--4.5-->
+<?php
+echo "<hr><h5>In task 5, you used SERVER; to get the browser name. 
+Use Switch statement with strops function to print the name of the browser as below: 
+If someone is using Chrome it should print, you are using Goolge Chrome….
+</h5>";
 
-
+?>
 <?php include "footer.php" ?>
